8b9808583b9ece90861d3290fd738f7a8bcc3c5d
kombo <kombomadou@gmail.com>
2023-05-03 13:09:32 +0000

    Suppression de donation list

--- src/app/donation/donation.module.ts
+++ src/app/donation/donation.module.ts
@@ -1,13 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { DonationListComponent } from './components/donation-list/donation-list.component';
 import { RouterModule, Routes } from '@angular/router';
 import { DonationTableComponent } from './components/donation-table/donation-table.component';
 import { SharedModule } from '../shared/shared.module';
 import { DonationService } from './services/donation.service';
-import { DonationsResolver } from './resolvers/donations.resolver';
-import { DonationNoAnonymousPersoResolver } from './resolvers/donation-no-anonymous-perso.resolver';
-import { DonationNoAnonymousOrgaResolver } from './resolvers/donation-no-anonymous-orga.resolver';
 import { ListAnonymousComponent } from './components/list-anonymous/list-anonymous.component';
 import { ListNoAnonymousPersoComponent } from './components/list-no-anonymous-perso/list-no-anonymous-perso.component';
 import { ListNoAnonymousOrgaComponent } from './components/list-no-anonymous-orga/list-no-anonymous-orga.component';
@@ -16,20 +12,13 @@ import { ListNoAnonymousPersoResolver } from './resolvers/list-no-anonymous-pers
 import { ListNoAnonymousOrgaResolver } from './resolvers/list-no-anonymous-orga.resolver';
 
 const donationRoutes: Routes = [
-  {path: 'liste', component: DonationListComponent, resolve: { 
-    donations: DonationsResolver, 
-    donationNoAnonymousPerso: DonationNoAnonymousPersoResolver,
-    donationNoAnonymousOrga: DonationNoAnonymousOrgaResolver
-  }}, 
   {path: 'anonyme', component: ListAnonymousComponent, resolve:{ listAnonymous: ListAnonymousResolver}},
   {path: 'non-anonyme/personel', component: ListNoAnonymousPersoComponent, resolve:{ listNoAnonymousPerso: ListNoAnonymousPersoResolver}},
   {path: 'non-anonyme/organisation', component: ListNoAnonymousOrgaComponent, resolve:{ listNoAnonymousOrga: ListNoAnonymousOrgaResolver}},
 ];
 
-
 @NgModule({
   declarations: [
-    DonationListComponent,
     ListAnonymousComponent,
     ListNoAnonymousPersoComponent,
     ListNoAnonymousOrgaComponent,
@@ -42,9 +31,6 @@ const donationRoutes: Routes = [
   ],
   providers:[
     DonationService, //Est en lazy load, sera chargé que si le module don est chargé
-    DonationsResolver,
-    DonationNoAnonymousPersoResolver,
-    DonationNoAnonymousOrgaResolver,
     ListAnonymousResolver,
     ListNoAnonymousPersoResolver,
     ListNoAnonymousOrgaResolver
