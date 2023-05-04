1d0774e7116febf7749ab7c23640e43ad4f897f0
kombo <kombomadou@gmail.com>
2023-05-04 15:39:16 +0000

    Ajoute de validation pour confirmation de mot de passe

--- src/app/admin/components/add-admin/add-admin.component.ts
+++ src/app/admin/components/add-admin/add-admin.component.ts
@@ -1,17 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-add-admin',
   templateUrl: './add-admin.component.html'
 })
-export class AddAdminComponent {
+export class AddAdminComponent implements OnInit {
   firstName!: string;
   lastName!: string;
   contact!: string;
   password!: string;
   confirmPassword!: string;
 
+  pwdIsConfirmed!: boolean;
+
+  ngOnInit(): void {
+    this.pwdIsConfirmed = true;
+  }
+
   onSubmit(){
 
+  }
+
+  onClickConfirmPassword(){
+    if(this.password != this.confirmPassword){
+      this.pwdIsConfirmed = false;
+    }
+    else{
+      this.pwdIsConfirmed = true;
+    }
   }
 }
