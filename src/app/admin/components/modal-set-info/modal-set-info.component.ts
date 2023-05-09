5eb2e8ae0722a9fda75385422b5c8cbc3e1cbd85
kombo <kombomadou@gmail.com>
2023-05-09 11:54:52 +0000

    Affichage des information de l'admin connecté dans profile

--- src/app/admin/components/modal-set-info/modal-set-info.component.ts
+++ src/app/admin/components/modal-set-info/modal-set-info.component.ts
@@ -1,9 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-modal-set-info',
   templateUrl: './modal-set-info.component.html'
 })
-export class ModalSetInfoComponent {
+export class ModalSetInfoComponent implements OnInit{
+  @Input() adminFirstName!: string|null;
+  @Input() adminLastName!: string|null;
+  @Input() adminContact!: string|null;
+  @Input() adminType!: string|null;
 
+  ngOnInit(): void {
+    console.log("dans le moda! "+ this.adminContact);
+    
+  }
 }
--- src/app/admin/components/profile/profile.component.ts
+++ src/app/admin/components/profile/profile.component.ts
@@ -1,9 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html'
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit{
+  adminFirstName!: string|null;
+  adminLastName!: string|null;
+  adminContact!: string|null;
+  adminType!: string|null;
+
+  ngOnInit(): void {
+    this.adminFirstName = sessionStorage.getItem('firstName');
+    this.adminLastName = sessionStorage.getItem('lastName');
+    this.adminContact = sessionStorage.getItem('contact');
+    this.adminType = sessionStorage.getItem('type');
+  }
 
 }
