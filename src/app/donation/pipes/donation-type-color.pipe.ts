8bf771e93e333b54db986d18861579dd6eef2ba1
kombo <kombomadou@gmail.com>
2023-05-11 20:01:56 +0000

    Correction de couleur des ligne de tableau dons ok

--- src/app/donation/pipes/donation-type-color.pipe.ts
+++ src/app/donation/pipes/donation-type-color.pipe.ts
@@ -5,20 +5,25 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class DonationTypeColorPipe implements PipeTransform {
 
-  transform(isAnonymous: number, isOrganisation: number): string {
-
-    if(isAnonymous === 1) { //anonymous
-      return "bg-secondary text-dark";
-    }
-    else if(isAnonymous === 0 && isOrganisation === 0) {//personal
-      return "bg-tertiary text-white";
-    }
-    else if(isAnonymous === 0 && isOrganisation === 1)//organisation
-    { 
-      return "bg-danger text-white";
+  transform(isAnonymous: number, isOrganisation: number, listType: string): string {
+    if(listType === "all"){
+      if(isAnonymous === 1) { //anonymous
+        return "bg-secondary text-dark";
+      }
+      else if(isAnonymous === 0 && isOrganisation === 0) {//personal
+        return "bg-tertiary text-white";
+      }
+      else if(isAnonymous === 0 && isOrganisation === 1)//organisation
+      { 
+        return "bg-danger text-white";
+      }
+      else{
+        return "";
+      }
     }
     else{
       return "";
     }
+
   }
 }
