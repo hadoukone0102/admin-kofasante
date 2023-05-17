bcdc1b0d523701db82312693167eadac1658d183
kombo <kombomadou@gmail.com>
2023-05-17 16:30:34 +0000

    Commentraire terminé dans le dashboard

--- src/app/admin/services/auth.service.ts
+++ src/app/admin/services/auth.service.ts
@@ -20,6 +20,13 @@ export class AuthService {
     private http: HttpClient,
     private coreService: CoreService) { }
 
+  /**
+   * Log in a administrator
+   * @date 5/17/2023 - 4:23:49 PM
+   *
+   * @param {Datalogin} dataLogin
+   * @returns {Observable<DataResultLogin>}
+   */
   login(dataLogin: Datalogin): Observable<DataResultLogin>{
     return this.http.post<DataResultLogin>(`${environment.apiUrlAdmin}/login`, dataLogin).pipe(
       tap((response) => console.log("ma reponse: "+ response.auth)
@@ -30,16 +37,33 @@ export class AuthService {
       }),)
     }
 
+    /**
+     * Return true if the administrator is authenticated
+     * @date 5/17/2023 - 4:25:09 PM
+     *
+     * @returns {boolean}
+     */
     isLogged(){
       const token = sessionStorage.getItem('token');
       return !! token; // !! transform the result to boolean
     }
 
+    /**
+     * Logout the administrator
+     * @date 5/17/2023 - 4:25:39 PM
+     */
     logout(){
       sessionStorage.clear();
       this.coreService.goToLogin();
     }
 
+  /**
+   * Send SMS to the administrator by his phone number
+   * @date 5/17/2023 - 4:25:53 PM
+   *
+   * @param {DataForgotPassword} contact
+   * @returns {Observable<DataResultForgotPassword>}
+   */
   sendSMS(contact: DataForgotPassword): Observable<DataResultForgotPassword>{
     const httpOptions = {
       headers: new HttpHeaders({ 'Content-type': 'application/json' })
@@ -55,6 +79,14 @@ export class AuthService {
     );
   }
 
+  /**
+   * Send confirmation code thar administrator has received by sms to the database
+   * and gets the confirmation
+   * @date 5/17/2023 - 4:26:44 PM
+   *
+   * @param {DataConfirmCode} smsCode
+   * @returns {Observable<DataResultConfirmCode>}
+   */
   sendConfirmationCode(smsCode: DataConfirmCode): Observable<DataResultConfirmCode> {
     return this.http.post<DataResultConfirmCode>(`${environment.apiUrlAdmin}/verification`, smsCode).pipe(
       tap((response) => console.log("c ok")
@@ -67,6 +99,13 @@ export class AuthService {
     );
   }
 
+  /**
+   * Reset administrator password 
+   * @date 5/17/2023 - 4:28:43 PM
+   *
+   * @param {DataResetPassword} newPassword
+   * @returns {Observable<DataResultResetPassword>}
+   */
   resetPassword(newPassword: DataResetPassword): Observable<DataResultResetPassword>{
     const httpOptions = {
       headers: new HttpHeaders({ 'Content-type': 'application/json' })
@@ -82,6 +121,12 @@ export class AuthService {
     );
   }
 
+  /**
+   * Get country code
+   * @date 5/17/2023 - 4:29:18 PM
+   *
+   * @returns {Observable<DataCountry>}
+   */
   getCountryCode(): Observable<DataCountry>{
     return this.http.get<DataCountry>(`${environment.apiUrlDon}/pays`).pipe(
       catchError((error: any) => {
